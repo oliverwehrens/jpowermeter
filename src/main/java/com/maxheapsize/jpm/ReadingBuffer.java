038f2ff573ca0d457b7b6dcf55b18c786c2296be
Oliver Wehrens <oliver@wehrens.de>
2015-09-10 21:48:30 +0200

    rounding mode

--- src/main/java/com/maxheapsize/jpm/ReadingBuffer.java
+++ src/main/java/com/maxheapsize/jpm/ReadingBuffer.java
@@ -18,11 +18,11 @@ public class ReadingBuffer {
 
     public SmartMeterReading getSmartMeterReadingInKwh() {
         SmartMeterReading smartMeterReadingInKwh = new SmartMeterReading();
-        smartMeterReadingInKwh.consumptionTotal.value = smartMeterReading.consumptionTotal.value.divide(new BigDecimal(1000));
+        smartMeterReadingInKwh.consumptionTotal.value = smartMeterReading.consumptionTotal.value.divide(new BigDecimal(1000), BigDecimal.ROUND_DOWN);
         smartMeterReadingInKwh.consumptionTotal.unit = "kWh";
-        smartMeterReadingInKwh.consumptionOne.value = smartMeterReading.consumptionOne.value.divide(new BigDecimal(1000));
+        smartMeterReadingInKwh.consumptionOne.value = smartMeterReading.consumptionOne.value.divide(new BigDecimal(1000), BigDecimal.ROUND_DOWN);
         smartMeterReadingInKwh.consumptionOne.unit = "kWh";
-        smartMeterReadingInKwh.consumptionTwo.value = smartMeterReading.consumptionTotal.value.divide(new BigDecimal(1000));
+        smartMeterReadingInKwh.consumptionTwo.value = smartMeterReading.consumptionTotal.value.divide(new BigDecimal(1000), BigDecimal.ROUND_DOWN);
         smartMeterReadingInKwh.consumptionTwo.unit = "kWh";
         smartMeterReadingInKwh.consumptionNow.value = smartMeterReading.consumptionNow.value;
         smartMeterReadingInKwh.consumptionNow.unit = smartMeterReading.consumptionNow.unit;
